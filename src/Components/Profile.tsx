cafcafffd25abf1376cbb2b01ffede93876b6ba5
Mirella Monteiro <mirellammoreira@hotmail.com>
2021-02-25 11:19:20 -0300

    Aula 1 e 2 NLW

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile () {
+    return (
+        <div>
+            <img src="https://twitter.com/MirellaMontei15" alt="Miella Monteiro" />
+            <div>
+                <strong> Mirella Monteiro</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import { ExperienceBar } from "../Components/ExpirenceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
